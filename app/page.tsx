8877f60bda694017407b75554d62d391f7f5d177
Samson Svendsen <samson.svendsen@vipps.no>
2024-06-04 07:10:16 +0200

    updating UI

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ const Check = () => (
     viewBox="0 0 24 24"
     strokeWidth={2}
     stroke="currentColor"
-    className="h-8 w-8 text-green-500"
+    className="h-8 w-8 text-cyan-400"
   >
     <path
       strokeLinecap="round"
@@ -20,65 +20,127 @@ const Check = () => (
   </svg>
 );
 
-const games = [{}, {}, {}];
-
-const completed = [{}, {}, {}];
-
 const Card: React.FC<{ children: React.ReactElement }> = ({ children }) => (
-  <div className="bg-neutral-600 rounded-md p-2">{children}</div>
+  <div className="bg-zinc-800 rounded-md p-2">{children}</div>
 );
 
 const Header3: React.FC<{ title: string }> = ({ title }) => (
-  <h3 className="text-2xl py-4">{title}</h3>
+  <h3 className="text-xl py-4 font-bold">{title}</h3>
 );
 
 type TeamScore = {
   team: string;
   score: number;
   rank: number;
-  firstPlaces: number[];
-  secondPlaces: number[];
-  thirdPlaces: number[];
+  firstPlaces: number;
+  secondPlaces: number;
+  thirdPlaces: number;
+  usedTokens: number;
 };
 
 const leaderBoard: TeamScore[] = [
   {
     team: "Team Gangbu",
-    score: 12,
-    rank: 2,
-    firstPlaces: [],
-    secondPlaces: [],
-    thirdPlaces: [],
+    score: 16,
+    rank: 1,
+    firstPlaces: 5,
+    secondPlaces: 2,
+    thirdPlaces: 3,
+    usedTokens: 2,
   },
   {
     team: "Team Chang",
-    score: 16,
-    rank: 1,
-    firstPlaces: [],
-    secondPlaces: [],
-    thirdPlaces: [],
+    score: 12,
+    rank: 2,
+    firstPlaces: 2,
+    secondPlaces: 3,
+    thirdPlaces: 2,
+    usedTokens: 0,
   },
   {
     team: "Team Badass",
     score: 11,
     rank: 3,
-    firstPlaces: [],
-    secondPlaces: [],
-    thirdPlaces: [],
+    firstPlaces: 0,
+    secondPlaces: 3,
+    thirdPlaces: 5,
+    usedTokens: 1,
   },
 ];
 
+const Medal: React.FC<{ color: string; textColor: string; pos: number }> = ({
+  color,
+  pos,
+  textColor,
+}) => (
+  <div
+    className={`rounded-full border-2 text-sm ${textColor} w-5 h-5 font-bold flex justify-center items-center ${color}`}
+  >
+    {pos}
+  </div>
+);
+
+const Token: React.FC<{ color: string; textColor: string }> = ({
+  color,
+  textColor,
+}) => (
+  <div
+    className={`rounded-full border-2 text-sm ${textColor} w-5 h-5 font-bold flex justify-center items-center ${color}`}
+  >
+    ?
+  </div>
+);
+
 const Leaderboard = () => (
   <div>
-    <Header3 title="Leaderboards" />
-
+    <div className="px-2">
+      <Header3 title="Leaderboard" />
+    </div>
     <div className="flex flex-col gap-2">
+      <div className="grid grid-cols-12 px-2 text-sm font-bold">
+        <div className="col-span-2">Rank</div>
+        <div className="col-span-8">Team</div>
+        <div className="col-span-2 flex justify-end">Points</div>
+      </div>
+
       {leaderBoard.map((l) => (
         <Card>
-          <div className="flex justify-between gap-6">
-            <div> {l.rank}</div>
-            <div className="grow">{l.team}</div>
-            <div> {l.score}</div>
+          <div className="grid grid-cols-12 px-4">
+            <div className="col-span-2 text-3xl font-bold flex items-center justify-start">
+              <div>{l.rank}</div>
+            </div>
+            <div className="col-span-8 flex flex-col gap-2">
+              <div className="text-md font-bold">{l.team}</div>
+              <div className="flex gap-1 flex-wrap">
+                {[...Array(l.firstPlaces)].map((_) => (
+                  <Medal
+                    pos={1}
+                    textColor="text-cyan-500"
+                    color="border-cyan-500"
+                  />
+                ))}
+                {[...Array(l.secondPlaces)].map((_) => (
+                  <Medal
+                    pos={2}
+                    textColor="text-pink-500"
+                    color="border-pink-500"
+                  />
+                ))}
+                {[...Array(l.thirdPlaces)].map((_) => (
+                  <Medal
+                    pos={3}
+                    textColor="text-zinc-400"
+                    color="border-zinc-400"
+                  />
+                ))}
+                {[...Array(l.usedTokens)].map((_) => (
+                  <Token textColor="text-zinc-100" color="border-zinc-800" />
+                ))}
+              </div>
+            </div>
+            <div className="col-span-2 font-bold flex justify-end items-center">
+              <div> {l.score}</div>
+            </div>
           </div>
         </Card>
       ))}
@@ -86,35 +148,175 @@ const Leaderboard = () => (
   </div>
 );
 
+const Triangle = () => (
+  <svg
+    className="w-8 h-8 stroke-cyan-500"
+    width="52"
+    height="46"
+    viewBox="0 0 52 46"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M28.1651 3.75L48.9497 39.75C49.9119 41.4167 48.7091 43.5 46.7846 43.5H5.21538C3.29089 43.5 2.08807 41.4167 3.05033 39.75L23.8349 3.74999C24.7972 2.08333 27.2028 2.08333 28.1651 3.75Z"
+      stroke-width="5"
+    />
+  </svg>
+);
+
+const Square = () => (
+  <svg
+    className="w-8 h-8 stroke-pink-500"
+    width="50"
+    height="50"
+    viewBox="0 0 50 50"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect x="2.5" y="2.5" width="45" height="45" rx="2.5" stroke-width="5" />
+  </svg>
+);
+
+const Circle = () => (
+  <svg
+    className="w-8 h-8 stroke-cyan-500"
+    width="52"
+    height="52"
+    viewBox="0 0 52 52"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle cx="26" cy="26" r="23.5" stroke="black" stroke-width="5" />
+  </svg>
+);
+
+type Game = {
+  title: string;
+  token?: "triangle" | "square" | "circle";
+  order: number;
+};
+
+const upcoming: Game[] = [
+  {
+    title: "The Cookie Cutter",
+    order: 5,
+  },
+  {
+    title: "The Cookie Cutter",
+    token: "triangle",
+    order: 6,
+  },
+  {
+    title: "The Cookie Cutter",
+    order: 7,
+  },
+  {
+    title: "The Cookie Cutter",
+    order: 8,
+  },
+  {
+    title: "The Cookie Cutter",
+    token: "square",
+    order: 9,
+  },
+];
+
+type CompletedGame = {
+  title: string;
+  tokenUsed?: "triangle" | "square" | "circle";
+  order: number;
+  winner: string;
+  second: string;
+  third: string;
+};
+
+const completed: CompletedGame[] = [
+  {
+    title: "The Cookie Cutter",
+    order: 4,
+    winner: "Team Gangbu",
+    second: "Team Cookie",
+    third: "Team Awesome",
+  },
+  {
+    title: "The Cookie Cutter",
+    tokenUsed: "triangle",
+    order: 3,
+    winner: "Team Gangbu",
+    second: "Team Cookie",
+    third: "Team Awesome",
+  },
+  {
+    title: "The Cookie Cutter",
+    order: 2,
+    winner: "Team Gangbu",
+    second: "Team Cookie",
+    third: "Team Awesome",
+  },
+  {
+    title: "The Cookie Cutter",
+    order: 1,
+    winner: "Team Gangbu",
+    second: "Team Cookie",
+    third: "Team Awesome",
+  },
+];
+
 const Games = () => (
   <div>
-    <Header3 title="Upcoming" />
+    <div className="px-2">
+      <Header3 title="Upcoming" />
+    </div>
 
     <div className="flex flex-col gap-2 mb-6">
-      {games.map((g) => (
+      {upcoming.map((u) => (
         <Card>
-          <div className="flex">
-            <div className="p-4 text-4xl">1</div>
-            <div className="p-4 grow">The Cookie Cutter</div>
-            {/* <div className="p-4">
-              <Check />
-            </div> */}
+          <div className="grid grid-cols-12 px-4">
+            <div className="col-span-2 text-3xl font-bold flex items-center justify-start">
+              <div>{u.order}</div>
+            </div>
+            <div className="col-span-7 flex items-center text-md font-bold">
+              {u.title}
+            </div>
+            <div className="col-span-3 flex justify-end">
+              {u.token === "circle" && <Circle />}
+              {u.token === "square" && <Square />}
+              {u.token === "triangle" && <Triangle />}
+            </div>
           </div>
         </Card>
       ))}
     </div>
 
-    <Header3 title="Completed" />
+    <div className="px-2">
+      <Header3 title="Completed" />
+    </div>
 
     <div className="flex flex-col gap-2 mb-4">
       {completed.map((g) => (
         <Card>
-          <div className="flex items-center gap-2 p-4 px-1">
-            <div className="">
+          <div className="grid grid-cols-12 px-4">
+            <div className="col-span-2 text-3xl font-bold flex items-center justify-start">
+              <div>{g.order}</div>
+            </div>
+            <div className="col-span-7 flex flex-col">
+              <div className="mb-1 text-md font-bold">{g.title}</div>
+              <div className="text-sm flex flex-col justify-center gap-.5 text-zinc-400">
+                {g.tokenUsed && (
+                  <Token textColor="text-zinc-100" color="border-zinc-800" />
+                )}
+                {!g.tokenUsed && (
+                  <>
+                    <div>1. {g.winner}</div>
+                    <div>2. {g.second}</div>
+                    <div>3. {g.third}</div>
+                  </>
+                )}
+              </div>
+            </div>
+            <div className="col-span-3 flex items-center justify-end">
               <Check />
             </div>
-            <div className="text-4xl">1</div>
-            <div className="grow">The Cookie Cutter</div>
           </div>
         </Card>
       ))}
@@ -123,17 +325,16 @@ const Games = () => (
 );
 
 const Index = () => {
-  const pathname = usePathname();
   const [currentTab, setCurrentTab] = useState<"games" | "leaderboard">(
     "games"
   );
   return (
     <div>
       <div className="flex justify-center">
-        <div className="p-1.5 rounded-lg bg-neutral-700">
+        <div className="p-1.5 rounded-lg bg-zinc-800">
           <button
             className={`min-w-24 p-2 rounded-lg ${
-              currentTab === "games" ? "bg-neutral-600" : ""
+              currentTab === "games" ? "bg-zinc-700" : ""
             }`}
             onClick={() => setCurrentTab("games")}
           >
@@ -141,7 +342,7 @@ const Index = () => {
           </button>
           <button
             className={`min-w-24 p-2 rounded-lg ${
-              currentTab === "leaderboard" ? "bg-neutral-600" : ""
+              currentTab === "leaderboard" ? "bg-zinc-700" : ""
             }`}
             onClick={() => setCurrentTab("leaderboard")}
           >
