a8d76250dd3d83b44c83c3b75f93357f341edaeb
Samson Svendsen <samson.svendsen@vipps.no>
2024-06-18 09:21:07 +0200

    adding auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,60 @@
 "use client";
+import { createContext, useEffect, useState } from "react";
 import "./globals.css";
 
 import { GeistSans } from "geist/font/sans";
+import { Session } from "@supabase/supabase-js";
+import { createClient } from "@/utils/supabase/client";
+import { redirect, usePathname } from "next/navigation";
+
+type AuthContextType = {
+  session: Session | null;
+};
+
+export const AuthContext = createContext<AuthContextType>({
+  session: null,
+});
+const supabase = createClient();
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [session, setSession] = useState<Session | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const pathname = usePathname();
+
+  console.log(pathname);
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+      setIsLoading(false);
+    });
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  if (!isLoading && !session) {
+    if (!pathname.endsWith("login") && pathname !== "/") {
+      redirect("/login"); // TODO: Should display login component directly here, and not redirect (causes flash)
+    }
+  }
+
   return (
     <html lang="en" className={GeistSans.className}>
       <body className="bg-zinc-900 text-foreground text-zinc-100">
-        {children}
+        {!isLoading && (
+          <AuthContext.Provider value={{ session }}>
+            {children}
+          </AuthContext.Provider>
+        )}
       </body>
     </html>
   );
--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,26 +1,17 @@
 "use client";
 
-import DeployButton from "@/components/DeployButton";
-import AuthButton from "@/components/AuthButton";
 import { createClient } from "@/utils/supabase/client";
-import FetchDataSteps from "@/components/tutorial/FetchDataSteps";
-import Header from "@/components/Header";
 import { redirect } from "next/navigation";
 import Link from "next/link";
+import { useContext } from "react";
+import { AuthContext } from "../layout";
 
-export default async function ProtectedPage() {
-  const supabase = createClient();
+export default function ProtectedPage() {
+  let { session } = useContext(AuthContext);
 
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
-
-  console.log("have fetched");
-  console.log(session);
-
-  if (!session) {
-    return redirect("/login");
-  }
+  // if (!isLoading && !session) {
+  //   return redirect("/login");
+  // }
 
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
@@ -31,18 +22,13 @@ export default async function ProtectedPage() {
         </div>
         <Link href="/dashboard">Test Dash</Link>
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-          <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-            <DeployButton />
-            <AuthButton />
-          </div>
+          <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm"></div>
         </nav>
       </div>
 
       <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
-        <Header />
         <main className="flex-1 flex flex-col gap-6">
           <h2 className="font-bold text-4xl mb-4">Next steps</h2>
-          <FetchDataSteps />
         </main>
       </div>
 
